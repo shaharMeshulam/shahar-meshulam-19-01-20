ac8b9d10b973e41dc29200230c72fb4d1ed50332
shaharMeshulam <shahar.mesh@gmail.com>
2020-01-22 19:27:46 +0200

    final commit

--- weather/src/app/components/header/header.component.ts
+++ weather/src/app/components/header/header.component.ts
@@ -1,25 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Store } from '@ngrx/store';
 
 import * as fromApp from '../../store/app.reducer';
 import * as ConfigActions from '../../store/config.actions';
 import { ThemeService } from 'src/app/theme/theme.service';
 import { take } from 'rxjs/operators';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, OnDestroy {
 
   selectedTheme: string;
   isCelsius: boolean;
 
+  private subscription: Subscription;
+
   constructor(private store: Store<fromApp.AppState>, private themeService: ThemeService) { }
 
   ngOnInit() {
-    this.store.select(fromApp.getTheme).subscribe(theme => this.selectedTheme = theme.name);
+    this.subscription = this.store.select(fromApp.getTheme).subscribe(theme => this.selectedTheme = theme.name);
 
     this.store.select(fromApp.getIsCelsius).pipe(
       take(1)
@@ -36,6 +39,10 @@ export class HeaderComponent implements OnInit {
 
   onToggleF2C() {
     this.store.dispatch(ConfigActions.toggleCelsius());
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 
 }
